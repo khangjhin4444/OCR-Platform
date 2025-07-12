d4c56cba253735f029056dc60ca03a334593c13e
Khang <nldkhang@gmail.com>
2025-07-12 20:04:44 +0700

    fix sort container render

--- front-end/src/pages/Document/DocumentsView.jsx
+++ front-end/src/pages/Document/DocumentsView.jsx
@@ -3,8 +3,10 @@ import "./DocumentsView.css";
 import DocumentTable from "./DocumentTable";
 
 export default function DocumentsView() {
-  const nameRef = useRef(null)
-  const dateRef = useRef(null)
+  const nameRef = useRef(null);
+  const dateRef = useRef(null);
+  const [inputSearch, setInputSearch] = useState("");
+
   let data = [
     {
       name: "a",
@@ -23,25 +25,35 @@ export default function DocumentsView() {
     },
   ];
 
-  const [tableData, setTableData] = useState(data)
+  const [tableData, setTableData] = useState(data);
 
   function handleChangeName(value) {
-    if (value === 'asc') {
-      setTableData(tableData => [...tableData].sort((a, b) => a.name.localeCompare(b.name)))
+    if (value === "asc") {
+      setTableData((tableData) =>
+        [...tableData].sort((a, b) => a.name.localeCompare(b.name))
+      );
     } else {
-      setTableData(tableData => [...tableData].sort((a, b) => b.name.localeCompare(a.name)))
+      setTableData((tableData) =>
+        [...tableData].sort((a, b) => b.name.localeCompare(a.name))
+      );
     }
-    dateRef.current.value = 'default'
+    dateRef.current.value = "default";
   }
 
   function handleChangeDate(value) {
-    if (value === 'asc') {
-      setTableData(tableData => [...tableData].sort((a, b) => new Date(a.date) - new Date(b.date)))
+    if (value === "asc") {
+      setTableData((tableData) =>
+        [...tableData].sort((a, b) => new Date(a.date) - new Date(b.date))
+      );
     } else {
-      setTableData(tableData => [...tableData].sort((a, b) => new Date(b.date) - new Date(a.date)))
+      setTableData((tableData) =>
+        [...tableData].sort((a, b) => new Date(b.date) - new Date(a.date))
+      );
     }
-    nameRef.current.value = 'default'
+    nameRef.current.value = "default";
   }
+
+  function handleSearch() {}
 
   return (
     <>
@@ -53,35 +65,63 @@ export default function DocumentsView() {
               type="text"
               className="search-input w-100"
               placeholder="Enter name or tag to search"
+              onChange={(event) => {
+                setInputSearch(
+                  (inputSearch) => (inputSearch = event.target.value)
+                );
+              }}
             />
           </div>
           <div className="col-sm-12 col-md-2">
-            <button className="search-btn w-100">Search</button>
+            <button className="search-btn" onClick={handleSearch}>
+              Search
+            </button>
           </div>
         </div>
       </div>
       <div className="sort-container mt-3 fs-5">
-        <p>Sort by:</p>
-        <div className="form-floating">
-          <select className="form-select" id="floatingSelectName" ref={nameRef} onChange={(event) => {handleChangeName(event.target.value)}}>
-            <option value="default" defaultValue={"default"}>
-              Default
-            </option>
-            <option value="asc">A - Z</option>
-            <option value="desc">Z - A</option>
-          </select>
-          <label htmlFor="floatingSelectName">Name</label>
-        </div>
-
-        <div className="form-floating">
-          <select className="form-select" id="floatingSelectDate" ref={dateRef} onChange={(event) => {handleChangeDate(event.target.value)}}>
-            <option value="default" defaultValue={"default"}>
-              Default
-            </option>
-            <option value="asc">ASC</option>
-            <option value="desc">DESC</option>
-          </select>
-          <label htmlFor="floatingSelectDate">Date</label>
+        <div className="row align-items-center">
+          <div className="col-sm-12 col-md-4">
+            <p>Sort by:</p>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <div className="form-floating">
+              <select
+                className="form-select"
+                id="floatingSelectName"
+                ref={nameRef}
+                onChange={(event) => {
+                  handleChangeName(event.target.value);
+                }}
+              >
+                <option value="default" defaultValue={"default"}>
+                  Default
+                </option>
+                <option value="asc">A - Z</option>
+                <option value="desc">Z - A</option>
+              </select>
+              <label htmlFor="floatingSelectName">Name</label>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <div className="form-floating">
+              <select
+                className="form-select"
+                id="floatingSelectDate"
+                ref={dateRef}
+                onChange={(event) => {
+                  handleChangeDate(event.target.value);
+                }}
+              >
+                <option value="default" defaultValue={"default"}>
+                  Default
+                </option>
+                <option value="asc">ASC</option>
+                <option value="desc">DESC</option>
+              </select>
+              <label htmlFor="floatingSelectDate">Date</label>
+            </div>
+          </div>
         </div>
       </div>
       <DocumentTable data={tableData} />
